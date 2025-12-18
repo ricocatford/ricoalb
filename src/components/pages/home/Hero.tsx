188326ba828483d51438ca212a019a2bf4f99b8f
ricocatford <rico.albarenque@gmail.com>
2025-12-18 02:25:06 +0100

    feat(home): add cards grid for layout testing

--- src/components/pages/home/Hero.tsx
+++ src/components/pages/home/Hero.tsx
@@ -1,7 +1,18 @@
+import styles from "@/assets/styles/components/pages/home/Hero.module.css";
+
 export const Hero = (): React.JSX.Element => {
     return (
         <section className="container">
-            <h1>Welcome bitches</h1>
+            <div className={styles.grid}>
+                <article className={styles.testCard}></article>
+                <article className={styles.testCard1}></article>
+                <article className={styles.testCard1}></article>
+                <article className={styles.testCard1}></article>
+                <article className={styles.testCard}></article>
+                <article className={styles.testCard1}></article>
+                <article className={styles.testCard}></article>
+                <article className={styles.testCard}></article>
+            </div>
         </section>
     );
 };
