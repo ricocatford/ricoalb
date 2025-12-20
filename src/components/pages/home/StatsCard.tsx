4790fe1e2cdf4fa859955d2110e7142a4314610b
ricocatford <rico.albarenque@gmail.com>
2025-12-21 00:53:55 +0100

    feat(stats card): add stats rendering

--- src/components/pages/home/StatsCard.tsx
+++ src/components/pages/home/StatsCard.tsx
@@ -7,10 +7,18 @@ interface StatsCardProps {
 }
 
 export const StatsCard = ({ section }: StatsCardProps) => {
+    console.log("Section stats: ", section.stats);
     return (
         <BentoCard variant="wide">
             <div className={styles.container}>
-                <p>Stats</p>
+                <dl className={styles.statsList}>
+                    {section.stats.map((stat) => (
+                        <div key={stat.id} className={styles.statItem}>
+                            <dt className={styles.statValue}>{stat.value}</dt>
+                            <dd className={styles.statLabel}>{stat.label}</dd>
+                        </div>
+                    ))}
+                </dl>
             </div>
         </BentoCard>
     );
