be72ebe50090bce05440b9087998815bc76764bd
ricocatford <rico.albarenque@gmail.com>
2026-05-08 20:00:23 +0200

    fix(services): cast type for services list

--- src/components/pages/services/ServicesList.tsx
+++ src/components/pages/services/ServicesList.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { useTranslations } from "@/providers/LanguageProvider";
-import styles from "@/assets/styles/components/pages/services/ServicesList.module.css";
 import { ServiceCard } from "./ServiceCard";
 import { Service } from "@/types/Service";
+import { asTranslations } from "@/lib/asTranslations";
+import styles from "@/assets/styles/components/pages/services/ServicesList.module.css";
 
 export const ServicesList = (): React.JSX.Element => {
     const { translations } = useTranslations();
-    const servicesList: Service[] = translations.common?.services?.list;
+    const services = asTranslations<{ list: Service[] }>(
+        translations.common?.services
+    );
+    const servicesList = services?.list;
 
     if (!Array.isArray(servicesList)) {
         return <div className={styles.container}>No services found.</div>;
